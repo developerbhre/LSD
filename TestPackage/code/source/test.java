669b2e275bd8dadbfbdcae57311514c907fc0e96
rbhargavnathreddy <rbargavanathareddy@softwareag.com>
2024-09-17 09:56:51 +0530

    Adding test package

---
+++ TestPackage/code/source/test.java
@@ -0,0 +1,58 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class test
+
+{
+	// ---( internal utility methods )---
+
+	final static test _instance = new test();
+
+	static test _newInstance() { return new test(); }
+
+	static test _cast(Object o) { return (test)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void java2 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(java2)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+
+
+
+	public static final void java3 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(java3)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+
+	// --- <<IS-START-SHARED>> ---
+	
+	private void testMethod(){
+		
+	}
+		
+	// --- <<IS-END-SHARED>> ---
+}
+
